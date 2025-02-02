f74c9ec531d9199c149bb080f76b7f7d7be1f797
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-02 02:14:48 -0600

    add SEO metadata to Pricing Page component

--- 01-first-steps/app/pricing/page.tsx
+++ 01-first-steps/app/pricing/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+ title: 'Princing Page',
+ description: 'This is service pricing page',
+};
 
 export default function PricingPage() {
     return (
