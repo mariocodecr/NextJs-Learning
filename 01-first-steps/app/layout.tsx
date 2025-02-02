4acfc1f35d17eb0ac63e0ea97f8bb62ee7c15019
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-02 02:35:51 -0600

    refactor layout to use Inter font and simplify body class

--- 01-first-steps/app/layout.tsx
+++ 01-first-steps/app/layout.tsx
@@ -1,33 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Inter} from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: "Mario's Home Page",
   description: "Generated with love by Vercel",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout ({ children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+          {children}
       </body>
     </html>
   );
