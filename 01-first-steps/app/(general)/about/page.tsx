16ae111460b8438e7f2253af2b83a5e5a22bd4b2
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-02 02:36:20 -0600

    add SEO metadata and basic structure for About Page component

---
+++ 01-first-steps/app/(general)/about/page.tsx
@@ -0,0 +1,15 @@
+
+export const metadata = {
+ title: 'SEO Title',
+ description: 'SEO Description',
+ keywords: ['About Page', 'Mario', 'Information', '...'],
+};
+
+export default function AboutPage() {
+
+    return (
+        <main className="flex flex-col items-center p-24">
+            <span className="text-7xl">About Page</span>
+        </main>
+    )
+}
