7ffa628f5db6e0f8f829b4b8fdfafa6c485719df
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-05 23:24:53 -0600

    refactor getPokemons function to return SimplePokemon array and update PokemonsPage component to display a Pokemon image

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,10 +1,18 @@
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
 
 
 
-const getPokemons = async (limit = 20, offset = 0) => {
-    const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+const getPokemons = async (limit = 20, offset = 0): Promise <SimplePokemon[]> => {
+    const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
     .then((res) => res.json());
-    return data;
+
+    const pokemons = data.results.map( pokemon => ({
+        id: pokemon.url.split('/').at(-2)!,
+        name: pokemon.name,
+    }))
+
+    return pokemons;
 }
 
 
@@ -13,11 +21,18 @@ const getPokemons = async (limit = 20, offset = 0) => {
 export default async function PokemonsPage() {
 
 
-    const pokemons = await getPokemons();
+    const pokemons = await getPokemons(151);
 
   return (
-    <div>
-      { JSON.stringify(pokemons) }
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg`}
+          width={100}
+          height={100}
+          alt="Name"
+        />
+      </div>
     </div>
   );
 }
