a76467cd01554cb486c9f2ee6b801290ce8d6fd6
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-06 00:23:38 -0600

    export PokemonGrid component from the pokemons module

--- 02-my-dashboard/src/app/pokemons/index.ts
+++ 02-my-dashboard/src/app/pokemons/index.ts
@@ -1,4 +1,8 @@
 
 
+export {PokemonGrid} from './components/PokemonGrid'
+
+
+
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
