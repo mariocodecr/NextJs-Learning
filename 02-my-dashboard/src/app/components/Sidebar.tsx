d69fa80af303174cb77b626f28c2c9f0761ff42e
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-05 22:42:23 -0600

    add Pokemons menu item to Sidebar with icon and subtitle

--- 02-my-dashboard/src/app/components/Sidebar.tsx
+++ 02-my-dashboard/src/app/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5";
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoLogoReact } from "react-icons/io5";
 import SidebarMenuItem from "./SidebarMenuItem";
 
 
@@ -20,6 +20,13 @@ const menuItems = [
     icon: <IoCalculator size={40} />,
     title: "Counter",
     subTitle: "Counter Client Side",
+  },
+
+  {
+    path: "/dashboard/pokemons",
+    icon: <IoFootball size={40} />,
+    title: "Pokemons",
+    subTitle: "Static generation",
   },
 ];
 
