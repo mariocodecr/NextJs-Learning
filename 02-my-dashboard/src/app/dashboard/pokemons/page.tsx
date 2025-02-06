6a1e747d5b8c896b37d23f9dec735a5589265a67
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-05 23:31:46 -0600

    update PokemonsPage component to dynamically display Pokemon images based on their IDs

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -26,12 +26,15 @@ export default async function PokemonsPage() {
   return (
     <div className="flex flex-col">
       <div className="flex flex-wrap gap-10 items-center justify-center">
-        <Image
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg`}
-          width={100}
-          height={100}
-          alt="Name"
-        />
+        {pokemons.map((pokemon) => (
+          <Image
+          key={ pokemon.id }
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemon.id}.svg`}
+            width={100}
+            height={100}
+            alt={ pokemon.name }
+          />
+        ))}
       </div>
     </div>
   );
