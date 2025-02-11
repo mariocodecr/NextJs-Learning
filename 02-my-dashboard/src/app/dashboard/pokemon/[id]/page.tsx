b49d9626ede709401bcbbeb6aa28abbdbf18e280
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-10 23:52:00 -0600

    feat: fetch and display individual Pokemon details in PokemonPage component

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,16 +1,32 @@
+import { Pokemon } from "@/pokemons";
 
 
 interface Props {
     params: { id: string };
 }
 
+const getPokemon = async (id: string): Promise <Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ id }`, {
+
+        cache: 'force-cache' // TODO change in future
+    }).then( resp => resp.json())
+
+    console.log(pokemon)
+
+    return pokemon
+}
 
 
-export default function PokemonPage({ params }: Props) {
+export default async function PokemonPage({ params }: Props) {
+
+    const pokemon = await getPokemon(params.id)
 
   return (
     <div>
       <h1> Pokemon { params.id } </h1>
+      <div>
+        { JSON.stringify(pokemon) }
+      </div>
     </div>
   );
 }
