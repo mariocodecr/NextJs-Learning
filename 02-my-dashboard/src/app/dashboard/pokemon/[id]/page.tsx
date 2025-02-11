24252bc2949499761514ee3caa080e0bf7fc4c4d
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-10 23:37:30 -0600

    feat: add PokemonPage component to display individual Pokemon details

---
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -0,0 +1,16 @@
+
+
+interface Props {
+    params: { id: string };
+}
+
+
+
+export default function PokemonPage({ params }: Props) {
+
+  return (
+    <div>
+      <h1> Pokemon { params.id } </h1>
+    </div>
+  );
+}
