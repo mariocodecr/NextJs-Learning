e0a3ac52238b7e40440c9a510af417bfc509e713
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-03-09 23:24:22 -0600

    feat: enhance MainPage layout with dashboard title and SimpleWidget component

--- 02-my-dashboard/src/app/dashboard/main/page.tsx
+++ 02-my-dashboard/src/app/dashboard/main/page.tsx
@@ -1,8 +1,17 @@
+import { SimpleWidget } from '../../../components/SimpleWidget';
 
 export default function MainPage() {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="text-black p-2">
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-xl">General Information</span>
+
+      <div className="flex flex-wrap p-2 items-center justify-center">
+
+        <SimpleWidget />
+    
+      </div>
+
     </div>
   );
 }
