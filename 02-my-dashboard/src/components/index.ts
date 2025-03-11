f4734d10be14060f699c91debb2e39e24215b53c
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-03-10 23:21:22 -0600

    feat: update component exports to use new directory structure for Sidebar and Widgets

--- 02-my-dashboard/src/components/index.ts
+++ 02-my-dashboard/src/components/index.ts
@@ -2,6 +2,8 @@
 
 
 
-export { default as Sidebar } from './Sidebar';
-export { default as SidebarMenuItem } from './SidebarMenuItem';
-export { SimpleWidget } from './SimpleWidget';
+
+export { default as Sidebar } from './sidebar/Sidebar';
+export { default as SidebarMenuItem } from './sidebar/SidebarMenuItem';
+export { SimpleWidget } from './dashboard/SimpleWidget';
+export { WidgetsGrid } from './dashboard/WidgetsGrid';
