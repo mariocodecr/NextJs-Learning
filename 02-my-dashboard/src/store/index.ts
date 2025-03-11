17cd9c0fab53c6880a99f6ecba4d57076d86a6cc
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-03-10 22:16:43 -0600

    feat: add typed hooks for useDispatch and useSelector in Redux store

--- 02-my-dashboard/src/store/index.ts
+++ 02-my-dashboard/src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 
 export const store = configureStore({
@@ -12,3 +13,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
