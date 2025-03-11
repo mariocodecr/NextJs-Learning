3768e7cb552d5f2a5fa669e1bec465d6978f40a8
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-03-10 23:55:35 -0600

    feat: fetch counter value from API and initialize state in CartCounter component

--- 02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
+++ 02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
@@ -8,15 +8,31 @@ interface Props {
   value?: number;
 }
 
+
+export interface CounterResponse {
+  method: string;
+  count: number;
+}
+
+const getApiCounter = async (): Promise<CounterResponse> => {
+    const data = await fetch('/api/counter').then( res => res.json());
+    return data;
+  }
+
 export const CartCounter = ({ value = 0 }: Props) => {
 
   const count = useAppSelector( state => state.counter.count );
   const dispatch = useAppDispatch();
 
-  useEffect( () => {
-    dispatch( initCounterState( value))
-  }, [dispatch, value]);
+  // useEffect( () => {
+  //   dispatch( initCounterState( value))
+  // }, [dispatch, value]);
 
+  useEffect( () => {
+    getApiCounter()
+      .then( ({ count }) => {dispatch( initCounterState( count ))
+    })
+  }, [dispatch]);
 
   return (
     <>
