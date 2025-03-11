120ac50514e4459d735912bde859877351b11615
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-03-10 22:44:05 -0600

    feat: integrate Redux for CartCounter state management and remove local state

--- 02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
+++ 02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
@@ -1,13 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { addOne, resetCount, substractOne } from "@/store/counter/counterSlice";
+import { useEffect, } from "react";
 
 interface Props {
   value?: number;
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-  const [count, setCount] = useState(value);
+
+  const count = useAppSelector( state => state.counter.count );
+  const dispatch = useAppDispatch();
+
+  useEffect( () => {
+    dispatch( resetCount( value))
+  }, [dispatch, value]);
+
+ 
+
+  //const [count, setCount] = useState(value);
 
   return (
     <>
@@ -15,14 +27,14 @@ export const CartCounter = ({ value = 0 }: Props) => {
 
       <div className="flex">
         <button
-          onClick={() => setCount(count + 1)}
+          onClick={() => dispatch(addOne () ) }
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
         >
           +1
         </button>
 
         <button
-          onClick={() => setCount(count - 1)}
+          onClick={() => dispatch( substractOne() ) }
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
         >
           -1
